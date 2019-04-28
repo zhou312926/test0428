bc8aa773068438b905b8f266868327071a79545a
zhou312926 <mathew_zhou@163.com>
2019-04-28 22:27:01 +0800

    0428

--- src/T0428/T0428.java
+++ src/T0428/T0428.java
@@ -4,5 +4,7 @@ public class T0428 {
     public static void main(String[] args) {
         System.out.println("0428");
         System.out.println("00000000");
+        System.out.println("1111111");
+        System.out.println("222222");
     }
 }
